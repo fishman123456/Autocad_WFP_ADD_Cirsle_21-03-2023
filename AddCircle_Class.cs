fb4659dbd824c0355512431ac581dea85d50c266
denis <106389581+fishman123456@users.noreply.github.com>
2023-08-04 11:45:47 +0700

    добавил текстбокс для диаметров

--- AddCircle_Class.cs
+++ AddCircle_Class.cs
@@ -14,74 +14,85 @@ namespace Autocad_WFP_ADD_Cirsle_21_03_2023
 {
    public class AddCircle_Class
     {
-        //[CommandMethod("AddCircle")]
+        [CommandMethod("AddCircle")]
 
-        //public static void AddCircle()
+        public  void AddCircle()
 
-        //{
+        {
+            WPF_Form WP_1 = new WPF_Form();
+            // Get the current document and database
 
-        //    // Get the current document and database
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
 
-        //    Document acDoc = Application.DocumentManager.MdiActiveDocument;
-
-        //    Database acCurDb = acDoc.Database;
-
-
-
-        //    // Start a transaction
-
-        //    using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
-
-        //    {
-
-        //        // Open the Block table for read
-
-        //        BlockTable acBlkTbl;
-
-        //        acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId,
-
-        //                                     OpenMode.ForRead) as BlockTable;
+            Database acCurDb = acDoc.Database;
 
 
 
-        //        // Open the Block table record Model space for write
+            // Start a transaction
 
-        //        BlockTableRecord acBlkTblRec;
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
 
-        //        acBlkTblRec = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace],
+            {
 
-        //                                        OpenMode.ForWrite) as BlockTableRecord;
+                // Open the Block table for read
 
+                BlockTable acBlkTbl;
 
+                acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId,
 
-        //        // Create a circle that is at 2,3 with a radius of 4.25
-
-        //        Circle acCirc = new Circle();
-
-        //        acCirc.SetDatabaseDefaults();
-
-        //        acCirc.Center = new Point3d(2, 3, 0);
-
-        //        acCirc.Radius = 4.25;
+                                             OpenMode.ForRead) as BlockTable;
 
 
 
-        //        // Add the new object to the block table record and the transaction
+                // Open the Block table record Model space for write
 
-        //        acBlkTblRec.AppendEntity(acCirc);
+                BlockTableRecord acBlkTblRec;
 
-        //        acTrans.AddNewlyCreatedDBObject(acCirc, true);
+                acBlkTblRec = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace],
+
+                                                OpenMode.ForWrite) as BlockTableRecord;
+
+                // мои попытки
+               
+
+                Circle acCirc = new Circle();
+
+                acCirc.SetDatabaseDefaults();
+
+                acCirc.Center = new Point3d(2, 3, 0);
+
+                acCirc.Radius = 4.25;
+
+
+                //// Create a circle that is at 2,3 with a radius of 4.25
+
+                //Circle acCirc = new Circle();
+
+                //acCirc.SetDatabaseDefaults();
+
+                //acCirc.Center = new Point3d(2, 3, 0);
+
+                //acCirc.Radius = 4.25;
 
 
 
-        //        // Save the new object to the database
+                // Add the new object to the block table record and the transaction
 
-        //        acTrans.Commit();
+                acBlkTblRec.AppendEntity(acCirc);
 
-        //    }
+                acTrans.AddNewlyCreatedDBObject(acCirc, true);
 
-        //}
-        public void help_1() {
+
+
+                // Save the new object to the database
+
+                acTrans.Commit();
+
+            }
+
+        }
+        public void help_get_list() {
+           
             WPF_Form WP_1 = new WPF_Form();
             WP_1.text_diam_plus();
                 }
--- WPF_Form.xaml.cs
+++ WPF_Form.xaml.cs
@@ -37,14 +37,14 @@ namespace Autocad_WFP_ADD_Cirsle_21_03_2023
             textplus();
             text_diam_plus();
         }
-        public  List <string> textplus()
+        public List<string> textplus()
         {
             int lineCount = TextBox1.LineCount;
 
             for (int line = 0; line < lineCount; line++)
                 // GetLineText takes a zero-based line index.
                 LayerCircl_name.Add(TextBox1.GetLineText(line));
-          
+
             return LayerCircl_name;
         }
         public List<string> text_diam_plus()
@@ -54,118 +54,31 @@ namespace Autocad_WFP_ADD_Cirsle_21_03_2023
             for (int line = 0; line < lineCount; line++)
                 // GetLineText takes a zero-based line index.
                 LayerCircl_diam.Add(TextBox2.GetLineText(line));
-
             return LayerCircl_diam;
         }
-
         private void B8_Click(object sender, RoutedEventArgs e)
         {
-           
-            
-
             foreach (var item in LayerCircl_name)
             {
                 //  MessageBox.Show(item.ToString());
 
                 broker_ one = new broker_();
                 one.list_broker = LayerCircl_name;
-                
             }
-
             foreach (var item in LayerCircl_diam)
             {
                 //  MessageBox.Show(item.ToString());
-
                 broker_ two = new broker_();
                 two.list_broker = LayerCircl_diam;
-
             }
-
-
         }
-
         private void TextBox1_TextChanged(object sender, TextChangedEventArgs e)
         {
-            LayerCircl_name.Add((string)TextBox1.Text);  
+            LayerCircl_name.Add((string)TextBox1.Text);
         }
         private void TextBox2_TextChanged(object sender, TextChangedEventArgs e)
         {
             LayerCircl_diam.Add((string)TextBox2.Text);
         }
-
-
-        // метод создания окружности
-        [CommandMethod("AddCircle")]
-
-        public static void AddCircle()
-
-        {
-
-            // Get the current document and database
-
-            Document acDoc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
-
-            Database acCurDb = acDoc.Database;
-
-
-
-            // Start a transaction
-
-            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
-
-            {
-
-                // Open the Block table for read
-
-                BlockTable acBlkTbl;
-
-                acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId,
-
-                                             OpenMode.ForRead) as BlockTable;
-
-
-
-                // Open the Block table record Model space for write
-
-                BlockTableRecord acBlkTblRec;
-
-                acBlkTblRec = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace],
-
-                                                OpenMode.ForWrite) as BlockTableRecord;
-
-
-
-                // Create a circle that is at 2,3 with a radius of 4.25
-
-                Circle acCirc = new Circle();
-
-                acCirc.SetDatabaseDefaults();
-
-                acCirc.Center = new Point3d(2, 3, 0);
-
-                acCirc.Radius = 4.25;
-
-
-
-                // Add the new object to the block table record and the transaction
-
-                acBlkTblRec.AppendEntity(acCirc);
-
-                acTrans.AddNewlyCreatedDBObject(acCirc, true);
-
-
-
-                // Save the new object to the database
-
-                acTrans.Commit();
-
-            }
-
-        }
-
-    
     }
-
-
-
 }
